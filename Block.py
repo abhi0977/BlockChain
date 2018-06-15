1b2c3fac17143cd0ae97a731e9c789a164a5ee00
Abhi Ramani <abhiramani3@gmail.com>
2018-06-15 16:18:28 +0530

    Add proof_of_work method

--- Block.py
+++ Block.py
@@ -10,13 +10,24 @@ class BlockChain:
         self.create_block(proof=1, previous_hash='0')
 
     def create_block(self, proof, previous_hash):
-        block = {'index' : len(self.chain) + 1,
-                 'timestamp' : str(datetime.datetime.now()),
-                 'proof' : proof,
-                 'previous_hash' : previous_hash}
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
         self.chain.append(block)
         return block
 
     def get_last_block(self):
         return self.chain[-1]
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
 
